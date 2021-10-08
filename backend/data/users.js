359c02d6743590ab6104fcb5a2e6c01a759567f7
square <artubr@gmail.com>
2021-10-08 02:35:49 +0200

    Add API endpoints.

--- backend/data/users.js
+++ backend/data/users.js
@@ -12,6 +12,11 @@ const pool = new Pool({
 module.exports = {
     getUsers: () => pool.query('SELECT * FROM users ORDER BY id ASC'),
     getUser: (id) => pool.query('SELECT * FROM users WHERE id = $1', [id]),
+    addUser: (username, password_hash, email, full_name, organization_id) =>
+        pool.query(
+            'INSERT INTO users (username, password_hash, email, full_name, organization_id) VALUES ($1, $2, $3, $4, $5)',
+            [username, password_hash, email, full_name, organization_id]
+        ),
     getOrganizations: () =>
         pool.query('SELECT * FROM organizations ORDER BY id ASC'),
     getOrganization: (id) =>
@@ -21,12 +26,32 @@ module.exports = {
             'INSERT INTO organizations (orgname, address, phone) VALUES ($1, $2, $3)',
             [orgname, address, phone]
         ),
-    addUser: (username, password_hash, email, full_name, organization_id) =>
-        pool.query(
-            'INSERT INTO users (username, password_hash, email, full_name, organization_id) VALUES ($1, $2, $3, $4, $5)',
-            [username, password_hash, email, full_name, organization_id]
-        ),
     getIngests: () => pool.query('SELECT * FROM ingests ORDER BY id ASC'),
+    getIngest: (id) => pool.query('SELECT * FROM ingests WHERE id = $1', [id]),
+    addIngest: (folder, job_id, description, user_id) =>
+        pool.query(
+            'INSERT INTO ingests (folder, job_id, description, user_id) VALUES ($1, $2, $3, $4)',
+            [folder, job_id, description, user_id]
+        ),
     getRelays: () => pool.query('SELECT * FROM relays ORDER BY id ASC'),
+    getRelay: (id) => pool.query('SELECT * FROM relay WHERE id = $1', [id]),
+    addRelay: (ingest_id, target_id, description, user_id, job_id) =>
+        pool.query(
+            'INSERT INTO relays (ingest_id, target_id, description, user_id, job_id) VALUES ($1, $2, $3, $4, $5)',
+            [ingest_id, target_id, description, user_id, job_id]
+        ),
     getTargets: () => pool.query('SELECT * FROM targets ORDER BY id ASC'),
+    getTarget: (id) => pool.query('SELECT * FROM relay WHERE id = $1', [id]),
+    addTarget: (
+        server,
+        stream_key,
+        description,
+        public_url,
+        platform,
+        user_id
+    ) =>
+        pool.query(
+            'INSERT INTO targets (server, stream_key, description, public_url, platform, user_id) VALUES ($1, $2, $3, $4, $5, $6)',
+            [server, stream_key, description, public_url, platform, user_id]
+        ),
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -91,6 +91,16 @@ router.get('/organizations', async (req, res) => {
     res.json(rows)
 })
 
+router.get('/organization/:id', async (req, res) => {
+    const { id } = req.params
+    const { rows } = await db.getOrganization(id)
+    if (rows.length === 1) {
+        res.json(rows[0])
+    } else {
+        res.json({ message: 'Organization not found' })
+    }
+})
+
 router.post('/organization/add', async (req, res) => {
     const { orgname, address, phone } = req.body
     const result = await db.addOrganization(orgname, address, phone)
@@ -102,14 +112,76 @@ router.get('/ingests', async (req, res) => {
     res.json(rows)
 })
 
+router.get('/ingest/:id', async (req, res) => {
+    const { id } = req.params
+    const { rows } = await db.getIngest(id)
+    if (rows.length === 1) {
+        res.json(rows[0])
+    } else {
+        res.json({ message: 'Ingest not found' })
+    }
+})
+
+router.post('/ingest/add', async (req, res) => {
+    const { folder, job_id, description, user_id } = req.body
+    const result = await db.addIngest(folder, job_id, description, user_id)
+    res.json({ mesage: 'Ingest added' })
+})
+
 router.get('/relays', async (req, res) => {
     const { rows } = await db.getRelays()
     res.json(rows)
 })
 
+router.get('/relay/:id', async (req, res) => {
+    const { id } = req.params
+    const { rows } = await db.getRelay(id)
+    if (rows.length === 1) {
+        res.json(rows[0])
+    } else {
+        res.json({ message: 'Relay not found' })
+    }
+})
+
+router.post('/relay/add', async (req, res) => {
+    const { ingest_id, target_id, description, user_id, job_id } = req.body
+    const result = await db.addRelay(
+        ingest_id,
+        target_id,
+        description,
+        user_id,
+        job_id
+    )
+    res.json({ mesage: 'Relay added' })
+})
+
 router.get('/targets', async (req, res) => {
     const { rows } = await db.getTargets()
     res.json(rows)
+})
+
+router.get('/target/:id', async (req, res) => {
+    const { id } = req.params
+    const { rows } = await db.getTarget(id)
+    if (rows.length === 1) {
+        res.json(rows[0])
+    } else {
+        res.json({ message: 'Target not found' })
+    }
+})
+
+router.post('/target/add', async (req, res) => {
+    const { server, stream_key, description, public_url, platform, user_id } =
+        req.body
+    const result = await db.addTarget(
+        server,
+        stream_key,
+        description,
+        public_url,
+        platform,
+        user_id
+    )
+    res.json({ mesage: 'Target added' })
 })
 
 module.exports = router
