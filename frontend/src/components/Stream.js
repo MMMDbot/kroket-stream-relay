171df09f702927c1c18e59a84518d91294330288
square <artubr@gmail.com>
2021-11-11 02:42:52 +0100

    Add multiselect to Stream component

--- frontend/src/components/Stream.js
+++ frontend/src/components/Stream.js
@@ -4,6 +4,7 @@ import Header from './Header'
 import Footer from './Footer'
 import StreamPlayer from './StreamPlayer'
 import Relays from './Targets'
+import MultiSelect from './MultiSelect'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -19,6 +20,7 @@ export default function Stream() {
                     <Col></Col>
                     <Col sm={12} md={10} lg={8}>
                         <StreamPlayer streamId={id} />
+                        <MultiSelect />
                         <Relays />
                     </Col>
                     <Col></Col>
