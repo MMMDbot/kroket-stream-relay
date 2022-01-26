cc2cc04bc0d55df60cc0031ebf5cf7394db439a7
square <artubr@gmail.com>
2022-01-26 01:23:11 +0100

    Add thumbnail dockerfile and typescript dep

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -25,7 +25,7 @@ const { downloadVideo } = require('../services/videoDownloader')
 const RedisStore = connectRedis(session)
 
 const redisClient = redis.createClient({
-    host: 'kroket-stream-relay_redis_1',
+    host: 'kroket-stream-relay-redis_1',
     port: 6379,
 })
 
