053fa2def9590412c594f2be8a0617ad7159ed9b
square <artubr@gmail.com>
2022-01-30 23:36:54 +0100

    Update Monitor text

--- frontend/src/components/Monitor.js
+++ frontend/src/components/Monitor.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 export default function Monitor() {
     return (
@@ -13,7 +14,12 @@ export default function Monitor() {
                 <Row>
                     <Col md={2}></Col>
                     <Col md={8}>
-                        <h1>Monitor</h1>
+                        <Card bsPrefix="card-shadow card-round mb-4">
+                            <Card.Body>
+                                <h1>Monitor</h1>
+                                <h2>Coming soon</h2>
+                            </Card.Body>
+                        </Card>
                     </Col>
                     <Col md={2}></Col>
                 </Row>
